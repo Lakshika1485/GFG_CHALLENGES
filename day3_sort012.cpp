48f1d07c90d99a74cafcb9a8bb66077a263e3f86
Lakshika1485 <143815506+Lakshika1485@users.noreply.github.com>
2024-09-27 22:19:04 +0530

    Add files via upload

---
+++ day3_sort012.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int sort012(vector<int>& nums) {
+        // code here
+        
+         sort(nums.begin(),nums.end());
+        	for(int i = 0; i<nums.size(); i++)
+        	{
+        		return nums[i];
+        	}
+        	cout<<" ";
+    }
+         
+   
+};
